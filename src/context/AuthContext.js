57f7bb9ac84b479f74c70ab1a0254cb33a8c362a
mh-munir <mhmunir914794@gmail.com>
2022-06-25 04:21:32 +0600

    netflix project

---
+++ src/context/AuthContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useEffect, useState} from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut, onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext()
+
+export function AuthContextProvider({children}){
+    const [user, setUser] = useState({});
+
+    function signUp(email, password){
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    return <AuthContext.Provider value={{signUp}} >{children}</AuthContext.Provider>
+}
+
+export function UserAuth(){
+    return useContext(AuthContext)
+}
